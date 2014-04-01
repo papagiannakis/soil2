1a7409b1f2db2b86f7e3524716785516c8331dad
Nicolas Guillemot <nlguillemot@gmail.com>
2014-03-31 22:45:38 -0700

    implemented cmake stuff

---
+++ cmake/Findsoil2.cmake
@@ -0,0 +1,8 @@
+# Defines:
+# soil2_FOUND - Always true
+# soil2_INCLUDE_DIR - The include directory for soil2
+# soil2_LIBRARY - The library for soil2
+
+set(soil2_FOUND TRUE)
+set(soil2_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../include)
+set(soil2_LIBRARY soil2)
